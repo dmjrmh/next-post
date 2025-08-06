d92b6e4bf78be0dbf4344b084e4bfc2e37c6d542
Dame <gban1mj27@gmail.com>
2025-08-06 13:40:58 +0700

    added fetching data posts

---
+++ src/app/components/Posts/CardList.tsx
@@ -0,0 +1,7 @@
+export const CardList = ({children} :any) => {
+  return (
+    <div className="bg-fuchsia-400 p-4 rounded-lg shadow-md">
+      {children}
+    </div>    
+  )
+}
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import styles from './postPage.module.css'
-import ViewUserButton from '../components/ViewUserButton'
+import ViewUserButton from '../components/Posts/ViewUserButton'
+import { CardList } from '../components/Posts/CardList'
 
-const Posts = () => {
+const base_url = "https://jsonplaceholder.typicode.com/posts"
+
+interface Post {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+const Posts = async () => {
+  const response = await fetch(base_url)
+  const posts: Post[] = await response.json()
+
   return (
     <>
     <div className={styles.bgRed}>Postingan Page</div>
-    <ViewUserButton />
+    <p>{posts[0].title}</p>
+    <CardList>
+      <p className='text-slate-700'>Hello</p>
+      <i>Hi</i>
+    </CardList>
     </>
   )
 }
